75c758e6def6ec11fd8e2934d3b50ebffd1fad78
ekzema <xxx.zp@mail.ru>
2017-06-06 02:00:33 +0300

    add link to cart

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
+  include CurrentCart
+  before_action :set_cart
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_catalog
   def index
     @products = Product.all
   end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,10 @@
 class Category < ApplicationRecord
 
   has_many :twocategories, :dependent => :destroy
-
+  validates :name, :presence => {message: 'Введите название категории'}
+  validates :name, :uniqueness => {message: 'Категория с таким именем уже существует'}
   has_attached_file :image, :default_url => "noimage.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   validates_attachment_file_name :image, matches: [/png\z/, /jpe?g\z/, /gif\z/]
+
 end

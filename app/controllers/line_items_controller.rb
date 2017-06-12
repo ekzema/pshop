41c02eae415e4ad4f8b4d163d04b46b69cbcc3c2
ekzema <ekzema@ekzema-VirtualBox.(none)>
2017-06-12 18:01:09 +0300

    add show cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,6 +1,6 @@
 class LineItemsController < ApplicationController
   include CurrentCart
-  before_action :set_cart, only: [:create]
+   before_action :set_cart, only: [:create, :destroy, :update]
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
 
   # GET /line_items
@@ -38,25 +38,24 @@ class LineItemsController < ApplicationController
 
   # PATCH/PUT /line_items/1
   # PATCH/PUT /line_items/1.json
-  def update
-    respond_to do |format|
-      if @line_item.update(line_item_params)
-        format.html { redirect_to @line_item, notice: 'Line item was successfully updated.' }
-        format.json { render :show, status: :ok, location: @line_item }
-      else
-        format.html { render :edit }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+ def update
+   if params[:line_item][:quantity] < "1"
+     render js: "alert('Колличество товара должно быть не меньше 1-ого!');"
+     else
+      respond_to do |format|
+        @line_item.update(line_item_params)
+       format.js
+     end
+   end
+ end
 
   # DELETE /line_items/1
   # DELETE /line_items/1.json
   def destroy
     @line_item.destroy
     respond_to do |format|
-      format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
-      format.json { head :no_content }
+      
+      format.js
     end
   end
 
@@ -68,6 +67,6 @@ class LineItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
+      params.require(:line_item).permit(:product_id, :quantity)
     end
 end

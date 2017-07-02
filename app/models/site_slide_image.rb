b9cf89fc0d00b84649d08ef20a1330ecee7b7f42
ekzema <Am2w3p4A>
2017-07-02 11:31:06 +0300

    design back2

--- app/models/site_slide_image.rb
+++ app/models/site_slide_image.rb
@@ -1,7 +1,7 @@
 class SiteSlideImage < ApplicationRecord
 
   belongs_to :site_setting
-  has_attached_file :image, :styles => {:showslide => "50X", :mainslide => "200X"}, :default_url => "noimage.png"
+  has_attached_file :image, :styles => {:showslide => "400X", :mainslide => "800X"}, :default_url => "noimage.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   validates_attachment_file_name :image, matches: [/png\z/, /jpe?g\z/, /gif\z/]
 
